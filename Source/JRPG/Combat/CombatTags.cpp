8ecaac8ff8ae078ee4a0f98a4f5ff3e644b80764
LeeDonghun <51253792+Leedonghun03@users.noreply.github.com>
2026-01-21 05:57:09 +0900

    [#5] 스킬에 사용될 CombatTags 코드 적용

---
+++ Source/JRPG/Combat/CombatTags.cpp
@@ -0,0 +1,24 @@
+﻿#include "CombatTags.h"
+
+namespace CombatTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Skill_Taunt,       "Combat.Skill.Taunt");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_AoeTaunt,    "Combat.Skill.AoeTaunt");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_Protect,     "Combat.Skill.Protect");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_ShieldAlly,  "Combat.Skill.ShieldAlly");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_MitSelf,     "Combat.Skill.MitSelf");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_CC,          "Combat.Skill.CC");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_BreakHelp,   "Combat.Skill.BreakHelp");
+
+	UE_DEFINE_GAMEPLAY_TAG(Skill_DpsHigh,     "Combat.Skill.DpsHigh");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_DpsLow,      "Combat.Skill.DpsLow");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_Break,       "Combat.Skill.Break");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_ThreatDown,  "Combat.Skill.ThreatDown");
+
+	UE_DEFINE_GAMEPLAY_TAG(Skill_HealSingle,  "Combat.Skill.HealSingle");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_HealAoE,     "Combat.Skill.HealAoE");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_Cleanse,     "Combat.Skill.Cleanse");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_MitAlly,     "Combat.Skill.MitAlly");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_Buff,        "Combat.Skill.Buff");
+	UE_DEFINE_GAMEPLAY_TAG(Skill_Debuff,      "Combat.Skill.Debuff");
+}
