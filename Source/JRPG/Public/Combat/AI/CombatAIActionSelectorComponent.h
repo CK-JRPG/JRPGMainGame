05b10c6bfda0af774f14c1d5f8c6e4e00bc47f36
YongseokPark <ssters999@outlook.kr>
2026-03-07 15:51:39 +0900

    [#12] 확정코드 적용 - 5

---
+++ Source/JRPG/Public/Combat/AI/CombatAIActionSelectorComponent.h
@@ -0,0 +1,47 @@
+﻿// Source/JRPGCombat/Public/Combat/AI/CombatAIActionSelectorComponent.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CombatAIActionSelectorComponent.generated.h" 
+
+class UBattleSessionSubsystem;
+class UCombatTargetingSubsystem;
+class USkillComponent;
+class USkillDataAsset;
+
+UCLASS(ClassGroup=(Combat), meta=(BlueprintSpawnableComponent))
+class JRPG_API UCombatAIActionSelectorComponent :public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UCombatAIActionSelectorComponent();
+
+	UPROPERTY(EditAnywhere) bool bAutoDriveEnemyTurns = true;
+	UPROPERTY(EditAnywhere) float ThinkDelaySec = 0.15f;
+
+	UPROPERTY(EditAnywhere) float HealThresholdRatio = 0.45f;
+	UPROPERTY(EditAnywhere) float EmergencyHealThresholdRatio = 0.25f;
+
+protected:
+	virtual void BeginPlay()override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	FTimerHandle ThinkTimer;
+
+	TWeakObjectPtr<USkillComponent> SkillComp;
+
+	UBattleSessionSubsystem* GetBattle() const;
+	UCombatTargetingSubsystem* GetTargeting() const;
+
+	void HandleTurnStarted(AActor *Actor,int32 Round);
+	void ThinkAndAct();
+
+	float GetHPRatio(AActor *Actor) const;
+	bool CanAffordSkill(const USkillDataAsset &Skill) const;
+
+	USkillDataAsset* PickBestHealSkill(TArray<AActor*> &OutTargets) const;
+	USkillDataAsset* PickBestOffensiveSkill(TArray<AActor*> &OutTargets) const;
+};
---
+++ Source/JRPG/Public/Combat/Battle/CombatTargetingSubsystem.h
@@ -0,0 +1,43 @@
+﻿// Source/JRPGCombat/Public/Combat/Battle/CombatTargetingSubsystem.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+
+#include "Combat/Battle/CombatTargetingTypes.h"
+#include "Combat/Skills/SkillDataAsset.h"
+
+#include "CombatTargetingSubsystem.generated.h"
+
+UCLASS()
+class JRPG_API UCombatTargetingSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	// 기본 공격용 “선호 타겟”
+	FTargetingResult ResolvePreferredBasicAttackTarget(AActor *Requester) const;
+
+	// 스킬용 “선호 타겟 세트”
+	FTargetingResult ResolvePreferredTargetsForSkill(AActor *Requester,const USkillDataAsset *Skill) const;
+
+	// 사용자가 고른 타겟이 합법적인지 검사
+	FTargetValidationResult ValidateSkillTargets(AActor *Requester,const USkillDataAsset *Skill,const TArray<AActor*> &Targets) const;
+
+	FTargetValidationResult ValidateBasicAttackTarget(AActor *Requester,AActor *Target)const;
+
+private:
+	class UBattleSessionSubsystem* GetBattle()const;
+
+	bool IsAliveCombatant(AActor *Actor) const;
+	bool IsSameTeam(AActor *A,AActor *B) const;
+	bool IsEnemyTeam(AActor *A,AActor *B) const;
+
+	float GetHPRatio(AActor *Actor) const;
+
+	AActor *PickTopThreatTarget(AActor *Requester,const TArray<AActor*> &Candidates) const;
+	AActor *PickLowestHPActor(const TArray<AActor*> &Candidates) const;
+	AActor *PickLowestHPAllyIncludingSelf(AActor *Requester,const TArray<AActor*> &Allies) const;
+
+	void GetAlliesIncludingSelf(AActor *Requester,TArray<AActor*> &Out) const;
+};
