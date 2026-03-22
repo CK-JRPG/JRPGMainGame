9f4cc27e7466e9617033a647bcc2b766ca204f22
hyerin <ku040423@gmail.com>
2026-03-22 10:51:43 +0900

    #63 필드에서 Player를 따라가는 CompanionPawn제작
    생성
    -JRPGCompanionPawn.h/.cpp
    -CompanionPawnController.h/.cpp
    
    -BP_CompanionPawnBase
    -BP_CompanionPawnAIController

---
+++ Source/JRPG/Private/Game/Companion/JRPGCompanionPawn.cpp
@@ -0,0 +1,46 @@
+﻿#include "Game/Companion/JRPGCompanionPawn.h"
+
+#include "Combat/Characters/CombatCharacterRegistrySubsystem.h"
+#include "Combat/Movement/JRPGCharacterMovementComponent.h"
+#include "Combat/Movement/LocomotionComponent.h"
+#include "Combat/Session/CombatZoneTrackerComponent.h"
+
+AJRPGCompanionPawn::AJRPGCompanionPawn(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UJRPGCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Locomotion = CreateDefaultSubobject<ULocomotionComponent>(TEXT("Locomotion"));
+	ZoneTracker = CreateDefaultSubobject<UCombatZoneTrackerComponent>(TEXT("CombatZoneTracker"));
+	
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+	
+	// AI 이동 방향을 자연스럽게 바라보게 하기 위함
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	if (UCharacterMovementComponent* MoveComp = GetCharacterMovement())
+	{
+		MoveComp->bOrientRotationToMovement = true;
+		MoveComp->RotationRate = FRotator(0.0f, 500.0f, 0.0f);
+		// AI가 플레이어를 따라갈 때 겹치지 않도록 RVO Avoidance 고려
+		// MoveComp->bUseRVOAvoidance = true; 
+	}
+}
+
+void AJRPGCompanionPawn::UpdateCharacter(FName NewCharId)
+{
+	CurrentCharacterId = NewCharId;
+	
+	// TODO: AnimInstance나 메쉬, 스켈레톤 변경 로직을 여기에 구현
+}
+
+ACombatCharacterActor* AJRPGCompanionPawn::GetCombatCharData() const
+{
+	UCombatCharacterRegistrySubsystem* RegistrySubsystem = GetGameInstance()->GetSubsystem<UCombatCharacterRegistrySubsystem>();
+	if (!RegistrySubsystem) 	
+		return nullptr;
+	
+	return Cast<ACombatCharacterActor>(RegistrySubsystem->FindById(CurrentCharacterId));
+}
---
+++ Source/JRPG/Public/Game/Companion/JRPGCompanionPawn.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Combat/Characters/CombatCharacterActor.h"
+#include "GameFramework/Character.h"
+#include "JRPGCompanionPawn.generated.h"
+
+class ULocomotionComponent;
+class UCombatZoneTrackerComponent;
+class ACombatCharacterActor;
+
+UCLASS()
+class JRPG_API AJRPGCompanionPawn : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AJRPGCompanionPawn(const FObjectInitializer& ObjectInitializer);
+
+	UPROPERTY(VisibleAnywhere, Category="JRPG")
+	TObjectPtr<ULocomotionComponent> Locomotion;
+
+	UPROPERTY(VisibleAnywhere, Category="JRPG")
+	TObjectPtr<UCombatZoneTrackerComponent> ZoneTracker;
+	
+	// 캐릭터 전투 데이터 식별용 ID
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "JRPG|Combat")
+	FName CurrentCharacterId;
+	
+	UFUNCTION(BlueprintCallable, Category = "JRPG|Combat")
+	void UpdateCharacter(FName NewCharId);
+	
+	UFUNCTION(BlueprintCallable, Category = "JRPG|Combat")
+	ACombatCharacterActor* GetCombatCharData() const;
+};
