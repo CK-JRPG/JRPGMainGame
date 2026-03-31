1881948e1a0a177754fe6428b80864cd4550be1c
hyerin <ku040423@gmail.com>
2026-03-31 19:25:34 +0900

    #87 Tactical 진입시 UI 적용
    수정
    JRPGHUD.h/cpp
    BP_JRPGHUD
    
    추가
    TacticalUIWidget.h/cpp
    BP_TacticalUI

---
+++ Source/JRPG/Public/UI/Combat/TacticalUIWidget.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TacticalUIWidget.generated.h"
+
+UCLASS()
+class JRPG_API UTacticalUIWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+};
