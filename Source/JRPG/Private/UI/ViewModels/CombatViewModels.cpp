de34dd707bffbb41219ad9ffd715548af3945333
hyerin <ku040423@gmail.com>
2026-04-02 09:56:53 +0900

    #96 CombatViewModels 작업중
    작업 완료
    Party Slot ViewModel
    
    작업 예정
    Enemy ViewModel
    Action Palette ViewModel

--- Source/JRPG/Private/UI/ViewModels/CombatViewModels.cpp
+++ Source/JRPG/Private/UI/ViewModels/CombatViewModels.cpp
@@ -1,5 +1,75 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "UI/ViewModels/CombatViewModels.h"
 
+#include "Combat/Stats/HPComponent.h"
+#include "Combat/Stats/APComponent.h"
+#include "Combat/SP/SPComponent.h"
+#include "Combat/Groggy/GroggyComponent.h"
+#include "GameFramework/Actor.h"
+
+// --- Party Slot ViewModel ---
+void UCombatPartySlotViewModel::BindToActor(AActor* MemberActor)
+{
+	Unbind();
+	if (!MemberActor) return;
+	OnNameUpdated.Broadcast(MemberActor->GetName());
+
+	if (UHPComponent* HPComp = MemberActor->FindComponentByClass<UHPComponent>())
+	{
+		CachedHPComp = HPComp;
+		HPComp->OnHPChanged.AddUObject(this, &UCombatPartySlotViewModel::HandleHPChanged);
+		HandleHPChanged(HPComp->GetHP(), HPComp->GetHP(), NAME_None);
+	}
+
+	if (UAPComponent* APComp = MemberActor->FindComponentByClass<UAPComponent>())
+	{
+		CachedAPComp = APComp;
+		APComp->OnAPChanged.AddUObject(this, &UCombatPartySlotViewModel::HandleAPChanged);
+		HandleAPChanged(APComp->GetAP(), APComp->GetAP(), NAME_None);
+	}
+}
+
+void UCombatPartySlotViewModel::Unbind()
+{
+	if (CachedHPComp.IsValid()) CachedHPComp->OnHPChanged.RemoveAll(this);
+	if (CachedAPComp.IsValid()) CachedAPComp->OnAPChanged.RemoveAll(this);
+}
+
+void UCombatPartySlotViewModel::HandleHPChanged(float OldHP, float NewHP, FName Reason)
+{
+	if (CachedHPComp.IsValid())
+	{
+		float MaxHP = CachedHPComp->MaxHP;
+		float Percent = MaxHP > 0.f ? NewHP / MaxHP : 0.f;
+		FString Text = FString::Printf(TEXT("%d / %d"), FMath::RoundToInt(NewHP), FMath::RoundToInt(MaxHP));
+		OnHPUIUpdated.Broadcast(Percent, Text);
+	}
+}
+
+void UCombatPartySlotViewModel::HandleAPChanged(int32 OldAP, int32 NewAP, FName Reason)
+{
+	if (CachedAPComp.IsValid())
+	{
+		float MaxAP = CachedAPComp->MaxAP;
+		float Percent = MaxAP > 0.f ? (float)NewAP / MaxAP : 0.f;
+		OnAPUIUpdated.Broadcast(Percent);
+	}
+}
+
+// --- Enemy ViewModel --- 
+void UEnemyViewModel::BindToEnemy(AActor* EnemyActor)
+{
+}
+
+
+void UEnemyViewModel::HandleGroggyCHanged(bool bGroggy)
+{
+}
+
+
+void UActionPalettedViewModel::BindToPlayer(AActor* PlayerActor)
+{
+}
+
+void UActionPalettedViewModel::HandleSPChanged(int32 OldSP, int32 NewSP, FName Reason)
+{
+}
--- Source/JRPG/Public/UI/ViewModels/CombatViewModels.h
+++ Source/JRPG/Public/UI/ViewModels/CombatViewModels.h
@@ -6,10 +6,70 @@
 /**
  *  1. ÆÄÆ¼ ½½·Ô ºä¸ðµ¨ (HP, AP Ã³¸®)
  */
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnNameUpdated, const FString&);
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnHPUIUpdated, float /*Percent*/, const FString& /*Text*/);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnAPUIUpdated, float /*Percent*/);
 
 UCLASS()
-class JRPG_API UCombatViewModels : public UObject
+class JRPG_API UCombatPartySlotViewModel : public UObject
 {
 	GENERATED_BODY()
-	
+public:
+	void BindToActor(AActor* MemberActor);
+	void Unbind();
+
+	FOnNameUpdated OnNameUpdated;
+	FOnHPUIUpdated OnHPUIUpdated;
+	FOnAPUIUpdated OnAPUIUpdated;
+
+private:
+	TWeakObjectPtr<class UHPComponent> CachedHPComp;
+	TWeakObjectPtr<class UAPComponent> CachedAPComp;
+
+	void HandleHPChanged(float OldHP, float NewHP, FName Reason);
+	void HandleAPChanged(int32 OldAP, int32 NewAP, FName Reason);
+};
+
+/// <summary>
+/// Àû/Å¸°Ù ºä¸ðµ¨ (HP, Groggy Ã³¸®)
+/// </summary>
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnTargetGroggyUpdated, bool /*bGroggy*/);
+
+UCLASS()
+class JRPG_API UEnemyViewModel : public UObject
+{
+	GENERATED_BODY()
+public:
+	void BindToEnemy(AActor* EnemyActor);
+	void Unbind();
+
+	FOnNameUpdated OnTargetNameUpdated;
+	FOnHPUIUpdated OnTargetHPUpdated;
+	FOnTargetGroggyUpdated OnTargetGroggyUpdated;
+
+private:
+	TWeakObjectPtr<class UHPComponent> CachedHPComp;
+	TWeakObjectPtr<class UGroggyComponent> CachedGroggyComp;
+
+	void HandleHPChanged(float OldHP, float NewHP, FName Reason);
+	void HandleGroggyCHanged(bool bGroggy);
+};
+
+/// <summary>
+/// ¾×¼Ç ÆÈ·¹Æ® ºä¸ðµ¨ (SP Ã³¸®)
+/// </summary>
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnSPUIUpdated, float /*Percent*/, const FString& /*Text*/);
+
+UCLASS()
+class JRPG_API UActionPalettedViewModel : public UObject
+{
+	GENERATED_BODY()
+public:
+	void BindToPlayer(AActor* PlayerActor);
+	void Unbind();
+	FOnSPUIUpdated OnSPUIUpdated;
+
+private:
+	TWeakObjectPtr<class USPComponent> CachedSPComp;
+	void HandleSPChanged(int32 OldSP, int32 NewSP, FName Reason);
 };
