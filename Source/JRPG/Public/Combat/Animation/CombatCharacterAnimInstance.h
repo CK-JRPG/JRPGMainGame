3ee18c87b0b4c31b5e448969dbdc3292daa04aa6
YongseokPark <ssters999@outlook.kr>
2026-04-10 20:43:47 +0900

    [#119] AnimInstanceBase C++ 구현 완료 - ABP로 확인 검사 해야함.

---
+++ Source/JRPG/Public/Combat/Animation/CombatCharacterAnimInstance.h
@@ -0,0 +1,81 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "Combat/Motion/CombatMotionTypes.h"
+#include "CombatCharacterAnimInstance.generated.h"
+
+class ACombatCharacterActor;
+class UCharacterMovementComponent;
+class UCombatMotionComponent;
+class UGroggyComponent;
+class UStatusEffectComponent;
+class UCombatActionComponent;
+class ULocomotionComponent;
+class UHPComponent;
+
+/*
+- 전투 캐릭터용 AnimInstance 베이스 클래스
+- ACombatCharacterActor 전용
+- 전투 컴포넌트(Groggy, Motion, HP 등)를 캐싱하여 ABP에 노출
+ */
+UCLASS()
+class JRPG_API UCombatCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|References")
+	TObjectPtr<ACombatCharacterActor> OwningCombatActor = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|References")
+	TObjectPtr<UCharacterMovementComponent> MovementComp = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|References")
+	TObjectPtr<UCombatMotionComponent> MotionComp = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|References")
+	TObjectPtr<UGroggyComponent> GroggyComp = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|References")
+	TObjectPtr<UStatusEffectComponent> StatusComp = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|References")
+	TObjectPtr<UCombatActionComponent> ActionComp = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|References")
+	TObjectPtr<ULocomotionComponent> LocomotionComp = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation|References")
+	TObjectPtr<UHPComponent> HPComp = nullptr;
+
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	float GroundSpeed = 0.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	float MovementDirection = 0.f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	bool bIsInAir = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	bool bIsAccelerating = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	bool bIsGroggy = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	bool bIsCombatMotionActive = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	EJRPGCombatMotionType ActiveMotionType = EJRPGCombatMotionType::None;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	bool bIsDead = false;
+};
